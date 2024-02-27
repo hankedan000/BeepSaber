adacb4ad987a81a7d0108bf847f279f733374925
hankedan000 <dany32412@gmail.com>
2024-02-27 08:33:33 -0500

    Handle maps that don't provide _difficultyLabel

--- game/BeepSaberMainMenu.gd
+++ game/BeepSaberMainMenu.gd
@@ -215,6 +215,8 @@ func mix_dificulty_sets(map_info):
 			if ds.get("_beatmapCharacteristicName") != "Standard":
 				if not d.has("_customData"):
 					d["_customData"] = {}
+				if not d["_customData"].has("_difficultyLabel"):
+					d["_customData"]["_difficultyLabel"] = ""
 				d["_customData"]["_difficultyLabel"] = (
 					str(ds.get("_beatmapCharacteristicName"))+" "+d["_customData"]["_difficultyLabel"])
 			newset.append(d)
